69280a971cc3a41684f8e231dc712233e4221884
Luhan Lacerda <luhanlacerda@gmail.com>
2016-10-19 10:05:01 -0300

    Ajustes das classes

--- src/aluno/Aluno.java
+++ src/aluno/Aluno.java
@@ -6,75 +6,19 @@
 package aluno;
 
 import classesBasicas.Endereco;
-import java.util.Date;
+import classesBasicas.Pessoa;
+import java.sql.Date;
 
 /**
  *
  * @author Luhan
  */
-public class Aluno {
+public class Aluno extends Pessoa {
     
-    private String nome;
-    private String rg;
-    private String cpf;
-    private Endereco endereco;
-    private Date dtnascimento;
     private int matricula;
     private Date dtmatricula;
-
-    public Aluno(String nome, String rg, String cpf, Endereco endereco, Date dtnascimento, int matricula, Date dtmatricula) {
-        this.nome = nome;
-        this.rg = rg;
-        this.cpf = cpf;
-        this.endereco = new Endereco();
-        this.dtnascimento = dtnascimento;
-        this.matricula = matricula;
-        this.dtmatricula = dtmatricula;
-    }
-
-    public Aluno (){
-        this.endereco = new Endereco();
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public Date getDtnascimento() {
-        return dtnascimento;
-    }
-
-    public void setDtnascimento(Date dtnascimento) {
-        this.dtnascimento = dtnascimento;
-    }
+    private float altura;
+    private float peso;
 
     public int getMatricula() {
         return matricula;
@@ -91,6 +35,23 @@ public class Aluno {
     public void setDtmatricula(Date dtmatricula) {
         this.dtmatricula = dtmatricula;
     }
-    
-    
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+
+
 }
